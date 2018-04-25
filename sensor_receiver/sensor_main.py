9f8edbfbbfcbb4b7c4d33bbf88c9538aa2239b41
Marcin <mkwas@student.agh.edu.pl>
2018-04-25 12:40:42 -0400

    Add function to handle sensor request.
    Function parse data from POST request
    of sensor and insert it to database.
    If we decide to split database into two
    tables we can check JSON header and choose
    correct table to insert it.

--- sensor_receiver/sensor_main.py
+++ sensor_receiver/sensor_main.py
@@ -1,21 +1,26 @@
 from mongoAccess import mongo3
 import datetime
+import json
+
+def parse_request(environ):
+
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except(ValueError):
+        request_body_size = 0
+
+    request_body = environ['wsgi.input'].read(request_body_size)
+    content = request_body.decode('utf-8')
+
+    return json.loads(content)
 
 def app(environ, start_response):
     status = '200 OK'
     db = mongo3.DB('', '', '172.17.0.2', 27017, "test", "myCollection")
-    currentTime = datetime.datetime.now()
-    db.insert(
-            {
-                "time": currentTime
-            })
-    outputStr = str(currentTime)
-    output = bytes(outputStr, 'utf-8')
 
+    db.insert(parse_request(environ))
+    response_headers = [('Content-type', 'text/plain')]
 
-    responseHeaders = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
 
-    start_response(status, responseHeaders)
-
-    return [output]
+    return["".encode()]
