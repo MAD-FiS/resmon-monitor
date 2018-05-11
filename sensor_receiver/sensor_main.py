a013a60bc0f49a7efe6be7105fe49406acdacb3e
Marcin <mkwas@student.agh.edu.pl>
2018-05-11 12:29:15 -0400

    Add proper JSON parsing.
    Change date format from string to datetime object.
    Remove field type before inserting JSON to database.

--- sensor_receiver/sensor_main.py
+++ sensor_receiver/sensor_main.py
@@ -2,7 +2,7 @@ from mongoAccess import mongo3
 import datetime
 import json
 
-def parse_request(environ):
+def parse_request(environ,db):
 
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
@@ -12,13 +12,21 @@ def parse_request(environ):
     request_body = environ['wsgi.input'].read(request_body_size)
     content = request_body.decode('utf-8')
 
-    return json.loads(content)
+    js = json.loads(content)
+    if js['TYPE'] == 'META':
+        db.insert(js)
+    elif js['TYPE'] == 'DATA':
+        for value in js['DATA']:
+            datetime_object = datetime.datetime.strptime(value['DATE'].split(".")[0], \
+                            '%Y-%m-%d %H:%M:%S')
+            value['DATE'] = datetime_object
+            db.insert(value)
 
 def app(environ, start_response):
     status = '200 OK'
     db = mongo3.DB('', '', '172.17.0.2', 27017, "test", "myCollection")
 
-    db.insert(parse_request(environ))
+    parse_request(environ,db)
     response_headers = [('Content-type', 'text/plain')]
 
     start_response(status, response_headers)
