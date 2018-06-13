20e3d3e10472e10d38bb0b12bbb0cb5b23ca3d74
IanRacon <rafalcosz@gmail.com>
2018-06-13 23:23:31 +0200

    Fix metadata inconsistency in project
    Change-Id: I931cd1bcddfa0ddde288792ba7c848d1e9bbc4cc

--- common/database/mongoAccess/dbApi.py
+++ common/database/mongoAccess/dbApi.py
@@ -12,7 +12,6 @@ class dbApi:
 
     DATA_COLL = "dataCollection"
     META_COLL = "metaCollection"
-    META2_COLL = "meta2Collection"
     NAME = "monitorDatabase"
     IP = "172.17.0.2"
     PORT = 27017
@@ -23,55 +22,46 @@ class dbApi:
         self.db = mongo3.DB(self.USER, self.PASSW,
                             self.IP, self.PORT, self.NAME)
 
-    DATA_KEY = "DATA"
-    SESSION_KEY = "SESSION_ID"
-    TIME_KEY = "DATE"
+    DATA_KEY = "data"
+    SESSION_KEY = "session_id"
+    TIME_KEY = "date"
     SESSION_PATH = DATA_KEY + "." + SESSION_KEY
     FROM = "$gt"
     TO = "$lt"
-    TYPE_KEY = "TYPE"
-    METRICS_KEY = "AVAILABLE_FIELDS"
-    METRIC_ID_KEY = "TAG"
+    TYPE_KEY = "type"
+    METRICS_KEY = "metrics"
+    METRIC_ID_KEY = "metric_id"
     METRIC_PATH = METRICS_KEY + "." + METRIC_ID_KEY
     METRIC_QUERY_KEY = "metric_id"
-    NAME_KEY = "NAME"
+    HOSTNAME_KEY = "hostname"
+    DESCRIPTION_KEY = "description"
 
     STANDARD_FIELDS = [
-        "METRICS",
-        "SESSION_ID",
-        "NAME",
-        "AVAILABLE_FIELDS",
-        "SESSION_START_DATE",
-        "DESCRIPTIONS",
+        "metrics",
+        "session_id",
+        "meta_descriptions",
+        "hostname",
+        "session_start_date",
+        "descriptions",
         "_id",
     ]
 
-    def findInMeta(self, filtr):
+    def findInMeta(self, filtr=None):
         return self.db.find(filtr, self.META_COLL)
-
-    def findInMeta2(self, filtr=None):
-        return self.db.find(filtr, self.META2_COLL)
 
     def findData(self, filtr):
         return self.db.find(filtr, self.DATA_COLL)
 
     def getSessionIds(self, dataFilter=None):
-        # TODO: delete once query metric_id
-        #       is consitent with database metric_id
         if dataFilter:
-            dbFilter = dataFilter.copy()
-            if self.METRIC_QUERY_KEY in dbFilter:
-                dbFilter[self.METRIC_PATH] = dbFilter[self.METRIC_QUERY_KEY]
-                del dbFilter[self.METRIC_QUERY_KEY]
-
-            metaEntries = self.findInMeta2(dbFilter)
+            metaEntries = self.findInMeta(dataFilter)
             return [entry[self.SESSION_KEY] for entry in metaEntries]
         else:
-            metaEntries = self.findInMeta2()
+            metaEntries = self.findInMeta()
             return [entry[self.SESSION_KEY] for entry in metaEntries]
 
     def getMetrics(self, sessionId, metricMatcher=None):
-        records = self.findInMeta2({self.SESSION_KEY: sessionId})
+        records = self.findInMeta({self.SESSION_KEY: sessionId})
         metrics = [record[self.METRICS_KEY] for record in records][0]
         print(metrics)
 
@@ -105,42 +95,42 @@ class dbApi:
         return self.db.select(self.DATA_COLL)
 
     def getAllMetrics(self):
-        dataEntries = self.db.find(None, self.META2_COLL)
+        dataEntries = self.findInMeta()
         response = {}
         for entry in dataEntries:
-            for metric in entry["AVAILABLE_FIELDS"]:
-                response.update({metric["TAG"]: metric["DESCRIPTION"]})
+            for metric in entry[self.METRICS_KEY]:
+                response.update({metric[self.METRIC_ID_KEY]: metric[self.DESCRIPTION_KEY]})
 
         return response
 
     def getHostname(self, sessionId):
-        return self.findInMeta2(
-            {self.SESSION_KEY: sessionId})[0][self.NAME_KEY]
+        return self.findInMeta(
+            {self.SESSION_KEY: sessionId})[0][self.HOSTNAME_KEY]
 
     def getHostnameByMetric(self, metric):
-        dataEntries = self.db.find(None, self.META2_COLL)
+        dataEntries = self.findInMeta()
         hostnames = []
         entries = {}
         for entry in dataEntries:
-            for met in entry["AVAILABLE_FIELDS"]:
-                if met["TAG"] == metric:
-                    hostnames.append(entry["NAME"])
+            for met in entry[self.METRICS_KEY]:
+                if met[self.METRIC_ID_KEY] == metric:
+                    hostnames.append(entry[self.HOSTNAME_KEY])
 
         return list(set(hostnames))
 
     def getHosts(self, query):
-        dataEntries = self.db.find(None, self.META2_COLL)
+        dataEntries = self.findInMeta()
         hostnames = []
         expression = re.compile(".*" + query + ".*")
         for entry in dataEntries:
-            if expression.match(entry["NAME"]):
-                hostnames.append(entry["NAME"])
+            if expression.match(entry[self.HOSTNAME_KEY]):
+                hostnames.append(entry[self.HOSTNAME_KEY])
 
         return list(set(hostnames))
 
     def getMetadataByHost(self, host):
         metadata = list()
-        metaEntries = self.findInMeta2({self.NAME_KEY: host})
+        metaEntries = self.findInMeta({self.HOSTNAME_KEY: host})
         for entry in metaEntries:
             for key in entry.keys():
                 if key not in self.STANDARD_FIELDS:
