49b56988d746a388daf060745f812041e410dd91
Łukasz Bartocha <bartocha.lukasz@gmail.com>
2018-06-14 06:56:18 +0000

    Revert "Add JWT authorization"
    This reverts commit 41f198b8e7ec7c4c5cb237e8ee9e233c956e5d30.
    
    Change-Id: Id89d7ca9308857c30d160fe2f5085574bf2b9a55

--- rest_api/swagger_server/controllers/metrics_controller.py
+++ rest_api/swagger_server/controllers/metrics_controller.py
@@ -1,10 +1,8 @@
 from rest_api.swagger_server.models.metric import Metric  # noqa: E501
 from rest_api.swagger_server import util
 from common.database.mongoAccess import dbApi
-from flask_jwt_extended import jwt_required
 
 
-@jwt_required
 def get_metrics():  # noqa: E501
     """List of metrics
 
