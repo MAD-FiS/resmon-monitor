de59e730b7f8d2e3fd46f065680634a87bc94a15
Tomasz Bajorek <tbajorek3@gmail.com>
2018-06-18 23:48:43 +0200

    Fix of installator and changes in README
    Change-Id: Ic7dee3d28c12698e7a5c957dfe47b62a051af040

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ cat ./scripts/install.template > install-monitor.sh
 echo "ARCHIVE_DATA:" >> install-monitor.sh
 echo "- Install file is created"
 
-tar -czvf tmp.tar.gz common config rest_api sensor_receiver .monitor-root __init__.py resmon-monitor.env README.md resmon-monitor >> /dev/null
+tar -czvf tmp.tar.gz common complex_meas_processor config rest_api sensor_receiver .monitor-root resmon-monitor resmon-monitor.env README.md >> /dev/null
 cat tmp.tar.gz >> install-monitor.sh
 rm tmp.tar.gz
 echo "- Required data is compressed and included"
