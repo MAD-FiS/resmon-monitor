48481dd6bf53fb83f48f498d0b5b8e3d0454d396
lukasz22 <bartocha.lukasz@gmail.com>
2018-06-18 14:13:08 +0200

    fix runtest exit code
    Change-Id: I357e3c298db9067a71a2d6d0230250d12fbdc302

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -6,4 +6,12 @@ if [ -z ${RESMONMONITORENV+x} ]; then
 fi
 
 python3.6 -m unittest discover -p "test*.py"
+EXIT_CODE1="$?"
 python3.6 -m unittest discover -p "*Test.py"
+EXIT_CODE2="$?"
+
+if [[ "$EXIT_CODE1" == "0" ]] && [[ "$EXIT_CODE2" == "0" ]]; then
+    exit 0
+else
+    exit 1
+fi
