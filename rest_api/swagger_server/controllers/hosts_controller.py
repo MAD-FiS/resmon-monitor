195a848d60d51166a86f0f765c8eebac6fdc314a
IanRacon <rafalcosz@gmail.com>
2018-06-19 00:46:04 +0200

    JWT-Authorization
    Change-Id: I569f94340940f7f5fb63072a414c54c283ceb55c

--- rest_api/swagger_server/controllers/hosts_controller.py
+++ rest_api/swagger_server/controllers/hosts_controller.py
@@ -14,7 +14,10 @@ from common.database.mongoAccess import dbApi
 from rest_api.swagger_server.controllers.metrics_controller import get_metrics
 from rest_api.apiUtils.apiUtils import QueryResolver
 
+from flask_jwt_extended import jwt_required
 
+
+@jwt_required
 def delete_metric(metric_id, hostname):  # noqa: E501
     """Delete complex metric
 
@@ -41,6 +44,7 @@ def delete_metric(metric_id, hostname):  # noqa: E501
     return "Success", 200
 
 
+@jwt_required
 def get_hosts(q=None):  # noqa: E501
     """Get list of hosts
 
@@ -82,6 +86,7 @@ def get_hosts(q=None):  # noqa: E501
     return response
 
 
+@jwt_required
 def post_metric(hostname, payload):  # noqa: E501
     """Add complex metric
 
