0a46de48b60cd8b0b3c5c3b6a9bb07883c1fcc52
Tomasz Bajorek <tbajorek3@gmail.com>
2018-06-21 00:21:15 +0200

    New information in README + configurable mongo connection + add ./data/ catalog
    Change-Id: I6e1170b93c8ad16b0a7505d814e975d211ede727

--- rest_api/monitor_main.py
+++ rest_api/monitor_main.py
@@ -3,6 +3,7 @@ import connexion
 import sys
 
 from flask_jwt_extended import JWTManager
+from rest_api.environment import ROOT_DIR
 
 
 def app(environ, start_fn):
@@ -16,22 +17,19 @@ def app(environ, start_fn):
     )
 
     try:
-        with open("/app/jwt.key") as f:
+        with open(ROOT_DIR + "/data/jwt.key") as f:
             key = f.read().rstrip()
     except FileNotFoundError:
         print(
-            'File with authorization key "jwt.key" not found',
+            'File with authorization key "./data/jwt.key" not found',
             "Monitor is unable to start",
         )
         sys.exit(
-            'File with authorization key "jwt.key" not found.'
+            'File with authorization key "./data/jwt.key" not found.'
             "Monitor is unable to start"
         )
 
-    # connApp.app.config["JWT_SECRET_KEY"] = key
-    connApp.app.config['JWT_SECRET_KEY'] = \
-        'eea637dfa8c5da0db6740b4b5708ee7aecc1d5f9f830d1ce7ab9df480bace474d8db8484b84ee'\
-        '4b16e8fbb490e0ac519caaec508379622abe051ba2a9dcda431'
+    connApp.app.config["JWT_SECRET_KEY"] = key
     jwt = JWTManager(connApp.app)
 
     return connApp.app(environ, start_fn)
